882fbf088754e45e024cc6719dad4c88f3331781
saurabh yadav <saurabh yadav@DESKTOP-MUGOT13>
2023-02-02 23:36:35 +0530

    okk

---
+++ javatest/src/javatest/JavaTest.java
@@ -0,0 +1,17 @@
+package javatest;
+
+public class JavaTest {
+	
+	public static void main(String[] args) {
+		int num=40;
+		switch(num)
+		{
+		case 5:System.out.println("five");
+		break;
+		case 35+5:System.out.println("forty");
+		break;
+		case 20+30:System.out.println("fifty");
+		}
+	
+		}
+		}
